636cd15853cc4604a5b0fb3d3f60dd368731f6c8
Claus Polanka <claus.polanka@erstegroup.com>
2025-04-22 19:58:37 +0200

    Remove comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,12 @@ java {
 repositories {
     mavenCentral()
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-    // nur für JARs, die wirklich nicht in Central/Sonatype liegen:
     flatDir {
         dirs("lib/deploy", "lib/develop")
     }
 }
 
 dependencies {
-    // → wirklich remote auflösbare Bibliotheken
     implementation("commons-io:commons-io:1.4")
     implementation("commons-lang:commons-lang:2.4")
     testImplementation("junit:junit:4.6")
@@ -28,11 +26,8 @@ dependencies {
     testImplementation("org.hamcrest:hamcrest-library:1.2")
     testImplementation("org.jmock:jmock:2.6.0")
 
-    // → alles, was nicht in Maven Central liegt, aus Deinem lib-Ordner:
     implementation(fileTree("lib/deploy") {
         include("*.jar")
-        // bei Bedarf bestimmte JARs ausschließen, z.B. Sources
-        // exclude("*-src.jar")
     })
     testImplementation(fileTree("lib/develop") {
         include("*.jar")
